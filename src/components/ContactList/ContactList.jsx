dd2f72fe57d0f19b3b959349e1d36fc3ad0abcb7
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-08-15 13:49:05 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -28,9 +28,9 @@ ContactList.propTypes = {
   contacts: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
-      name: PropTypes.string,
-      number: PropTypes.string,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
     })
-  ),
+  ).isRequired,
   deleteContact: PropTypes.func.isRequired,
 };
